e2011d52ea308adcf1064799890088efcd4f8120
Abhishek <abhishek1010mukherjee@gmail.com>
2018-05-06 22:31:02 +0530

    second commit

---
+++ MySOAPDemo/src/main/java/com/abhishek/mysoapdemo/MovieCatalog.java
@@ -0,0 +1,13 @@
+package com.abhishek.mysoapdemo;
+
+public class MovieCatalog {
+
+	//This Web Service was created using Eclipse Bottom Up Approach, by selecing this
+	//class and right-clicking to select WebService (Apache Axis WS).
+	
+	public String bestPicture(){
+		
+		return "The Shape of Water";
+		
+	}
+}
